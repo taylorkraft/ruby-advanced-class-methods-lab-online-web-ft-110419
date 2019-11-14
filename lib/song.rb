076022cb1fd0dd5a4618a2fa25dd5a2d817ad67a
Taylor Kraft <taylor.kraft19@gmail.com>
2019-11-14 22:17:07 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -37,15 +37,19 @@ class Song
     @@all.sort_by {|song|song.name}
   end
   
-  def self.new_from_filename
+  def self.new_from_filename(name)
     song = self.new 
     song.name = (name.split(" - ")[1].chomp(".mp3"))
     song.artist_name = (name.split(" - ")[0])
     song
   end 
     
-  def self.create_from_filename
-    
+  def self.create_from_filename(name)
+    song = self.new
+    song.name = (name.split(" - ")[1].chomp(".mp3"))
+    song.artist_name = (name.split(" - ")[0])
+    @@all << song
+    song
   end
 
   def self.destroy_all
