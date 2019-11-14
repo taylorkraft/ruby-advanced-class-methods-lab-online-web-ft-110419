e762638d6c9041a1e3e83ae477936f4f4e0767ee
Taylor Kraft <taylor.kraft19@gmail.com>
2019-11-14 20:35:21 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -26,10 +26,7 @@ class Song
   end
   
   def self.find_or_create_by_name(name)
-    song = self.new
-    song.name = name
-    self.all.find{|song| song.name == name}
-    @@all << song 
+    self.find_by_name || self.create_by_name
   end
   
   def self.all
