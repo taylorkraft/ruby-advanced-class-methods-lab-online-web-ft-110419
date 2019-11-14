e7019ee6708ea13f5de8190984a3ae64b699551e
Taylor Kraft <taylor.kraft19@gmail.com>
2019-11-14 20:04:17 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -8,9 +8,9 @@ class Song
     song
   end
   
-  def self.new_by_name
-    song = self.new
-    song.name = song
+  def self.new_by_name(song)
+    name = self.new
+    song.name = name
   end
   
   def self.all
