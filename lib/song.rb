c5a6acb257505fa7fff161abcbba5703117bae3d
Taylor Kraft <taylor.kraft19@gmail.com>
2019-11-14 19:34:08 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -2,7 +2,7 @@ class Song
   attr_accessor :name, :artist_name
   @@all = []
 
-  def self.create(song)
+  def self.create(song_name)
     song = song.new
     song.name = song 
     song = Song.create
